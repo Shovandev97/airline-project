f0c1d2e7ad1d29818051c3cb1f1f7d97553bb1ac
Shovan <Shovan@192.168.0.103>
2022-03-17 11:57:35 +0530

    Saha Commit

---
+++ airLine/src/main/java/com/airline/AirLineApplication.java
@@ -0,0 +1,14 @@
+package com.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirLineApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
